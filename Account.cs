714e48443705c12edbb0f365c2d4854487553914
PurpleLightFip <fip.purple@gmail.com>
2015-09-24 00:56:33 +0300

    Revert "Revert "Защита данных""
    This reverts commit 461bdde7156bdc151971f5a6e1e784f94f49bc05.

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLab_DataProtecting
+{
+  
+    public class Account
+    {
+        public string bName;
+        public string bPassword;
+        public bool bLock;
+        public bool bRestrictionPassword;
+        public Account() { } 
+    }       
+}
